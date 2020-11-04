c760b753a4a59c7b1d622293d9a85d6a035f0772
Saurabh Shanbhag <saurabhshanbhag@Saurabhs-MacBook-Pro.local>
2020-11-04 02:05:25 -0800

    Add airport data

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+import csv
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    airports = getAirports()
+    return render_template('index.html', airports=airports)
+
+def getAirports():
+    airports = []
+    with open('static/airport_data.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            airports.append(row[4] +" - "+row[1])
+
+    return sorted(airports)
 
 if __name__ == "__main__":
     app.run(debug=True)
