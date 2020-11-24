35d628bc20ffe1ba60d52ce7808c231b133181ee
Saurabh Shanbhag <saurabhshanbhag@Saurabhs-MacBook-Pro.local>
2020-11-23 22:58:05 -0800

    Fix web driver

--- flights.py
+++ flights.py
@@ -1,16 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.expected_conditions import visibility_of_element_located
-from selenium.webdriver.common.by import By
+
 import time
 import json
+import os
 
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-
-chrome_options = Options()  
-chrome_options.add_argument("--headless")  
 
 class Flight:
 	def __init__(self, cost, duration, stops):
@@ -25,19 +21,10 @@ class Flight:
 	def to_string(self):
 		return self.__dict__
 
-def scan_skiplagged(alert, debug):
-	if debug == True:
-		driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
-	else:
-		chrome_options.add_argument('--disable-gpu')
-		chrome_options.add_argument('--no-sandbox')
-		#chrome_options.binary_location = GOOGLE_CHROME_PATH
-		driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=chrome_options)
-
+def scan_skiplagged(alert, debug, driver):
 	url = "https://skiplagged.com/flights/"+alert.source+"/"+alert.destination+"/"+alert.departure_range[0]+"/"+alert.arrival_range[0]
 	print(url)
 	driver.get(url)
-	WebDriverWait(driver, 100).until(visibility_of_element_located((By.XPATH, '/html/body/section/div/div/section/div/div/div/div[2]/div/div[2]/div[3]/div[6]/div/div[2]/div[3]/p')))
 	time.sleep(10)
 
 	flights = []
@@ -64,4 +51,15 @@ def scan_skiplagged(alert, debug):
 	return flights
 
 def get_valid_flights(alert, debug):
-	return scan_skiplagged(alert, debug)
+	chrome_options = webdriver.ChromeOptions()  
+	chrome_options.add_argument("--headless")  
+	if debug == True:
+		driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
+	else:
+		chrome_options.add_argument('--disable-gpu')
+		chrome_options.add_argument('--disable-dev-shm-usage')
+		chrome_options.add_argument('--no-sandbox')
+		chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+		driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+	return scan_skiplagged(alert, debug, driver)
