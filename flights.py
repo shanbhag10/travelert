71e833d26b892be9adfeda0f2053db025165f6b2
Saurabh Shanbhag <saurabhshanbhag@Saurabhs-MacBook-Pro.local>
2020-11-26 01:58:28 -0800

    Update to css selector

--- flights.py
+++ flights.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 import time
 import json
 import os
-
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 class Flight:
 	def __init__(self, cost, duration, stops):
@@ -21,6 +17,7 @@ class Flight:
 	def to_string(self):
 		return self.__dict__
 
+
 def scan_skiplagged(alert, debug, driver):
 	url = "https://skiplagged.com/flights/"+alert.source+"/"+alert.destination+"/"+alert.departure_range[0]+"/"+alert.arrival_range[0]
 	print(url)
@@ -28,32 +25,34 @@ def scan_skiplagged(alert, debug, driver):
 	time.sleep(10)
 
 	flights = []
-	for i in range (2,11):
-		try:
-			cost_text = driver.find_element_by_xpath('/html/body/section/div/div/section/div/div/div/div[2]/div/div[2]/div[3]/div[6]/div/div['+str(i)+']/div[3]/p')
-			duration = driver.find_element_by_xpath('/html/body/section/div/div/section/div/div/div/div[2]/div/div[2]/div[3]/div[6]/div/div['+str(i)+']/div[1]')
+	try:
+		costs = driver.find_elements_by_css_selector('div.trip-cost')
+		durations = driver.find_elements_by_css_selector('div.trip-duration')
+	except Exception as error:
+		print("Error: " + str(error))
+		return flights
 
-			#button = driver.find_element_by_xpath('/html/body/section/div/div/section/div/div/div/div[2]/div/div[2]/div[4]/div[7]/div/div[2]/div[3]/button[1]').click()
+	for i in range(len(costs)):
+		#cost_text = driver.find_element_by_xpath('/html/body/section/div/div/section/div/div/div/div[2]/div/div[2]/div[3]/div[6]/div/div['+str(i)+']/div[3]/p')
+		#duration = driver.find_element_by_xpath('/html/body/section/div/div/section/div/div/div/div[2]/div/div[2]/div[3]/div[6]/div/div['+str(i)+']/div[1]')
+		#button = driver.find_element_by_xpath('/html/body/section/div/div/section/div/div/div/div[2]/div/div[2]/div[4]/div[7]/div/div[2]/div[3]/button[1]').click()
 
-			cost = int(cost_text.text[1:])
-			details = duration.text.split()
-			if len(details) > 3:
-				details = details[2:]
+		cost = int(costs[i].text[1:])
+		details = durations[i].text.split()
+		if len(details) > 3:
+			details = details[2:]
 
-			flight = Flight(cost, details[0], details[1])
-			print(flight.to_string())
-			flights.append(flight)
-
-		except Exception as error:
-			print("error" + str(error))
-			break
+		flight = Flight(cost, details[0], details[1])
+		print(flight.to_string())
+		flights.append(flight)
 
 	return flights
 
 def get_valid_flights(alert, debug):
 	chrome_options = webdriver.ChromeOptions()  
 	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument("--headless")  
+	chrome_options.add_argument("--headless") 
+	
 	if debug == True:
 		driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
 	else:
